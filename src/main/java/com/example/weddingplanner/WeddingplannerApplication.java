f88abc80ce9fbef16c6952d2a0686503d714feb8
wwwLe404 <duschl.lea@gmail.com>
2026-05-05 15:26:33 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/example/weddingplanner/WeddingplannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.weddingplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingplannerApplication.class, args);
+	}
+
+}
