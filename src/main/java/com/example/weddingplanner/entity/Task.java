f9222b5c00dd1086958e7bec2dc8cddd7bb19a95
wwwLe404 <duschl.lea@gmail.com>
2026-05-07 11:16:46 +0200

    Add Task entity with WeddingPlan relationship

---
+++ src/main/java/com/example/weddingplanner/entity/Task.java
@@ -0,0 +1,55 @@
+package com.example.weddingplanner.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "wedding_plan_id")
+    private WeddingPlan weddingPlan;
+
+    public Task() {
+    }
+
+    public Task(String title, boolean completed, WeddingPlan weddingPlan) {
+        this.title = title;
+        this.completed = completed;
+        this.weddingPlan = weddingPlan;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public WeddingPlan getWeddingPlan() {
+        return weddingPlan;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public void setWeddingPlan(WeddingPlan weddingPlan) {
+        this.weddingPlan = weddingPlan;
+    }
+}
